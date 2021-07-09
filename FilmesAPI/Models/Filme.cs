0b6a0e3494af71192f2412cef6e8c717398205a0
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-07-09 06:50:07 -0300

    Validando parametros recebidos

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo título não pode ser vazio")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor não pode ser vazio")]
         public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O tamanho do genêro não pode ser superior a 30")]
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve estar entre 1 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
